9a14f9aa0d5a46d1dc10d4edddcf843126bf96e9
Абдуллаева Надежда <abdullaeva.nadya494a@gmail.com>
2022-06-17 22:27:32 +0700

    Первоначальная инициализация проекта.

---
+++ src/test/java/ru/abdullaeva/javacoursework/JavacourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abdullaeva.javacoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
